5f2be4360b2425749b2c15411854c8e8a08936c7
tdakkota <tanc13@yandex.ru>
2020-10-08 21:44:12 +0300

    fix: add file filters for JFileChooser

--- src/main/java/ru/ncedu/tdakkota/app/ArchiveComparatorApp.java
+++ src/main/java/ru/ncedu/tdakkota/app/ArchiveComparatorApp.java
@@ -4,15 +4,21 @@ import ru.ncedu.tdakkota.archive.ZipArchive;
 import ru.ncedu.tdakkota.comparator.ArchiveComparator;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.IOException;
 
 public class ArchiveComparatorApp extends JFrame {
-    private final String[][] FILTERS = {{"zip", "ZIP Archive (*.zip)"}};
+    private final String[][] FILTERS = {{"zip", "ZIP Archive (*.zip)", "jar", "Java Archive (*.jar)"}};
 
     private File askFile() throws IOException {
         JFileChooser c = new JFileChooser();
         c.setMultiSelectionEnabled(false);
+        for (int i = 0; i < FILTERS[0].length; i++) {
+            FileNameExtensionFilter eff = new FileNameExtensionFilter(FILTERS[i][0], FILTERS[i][1]);
+            c.addChoosableFileFilter(eff);
+        }
+
         int ret = c.showOpenDialog(this);
         if (ret != JFileChooser.APPROVE_OPTION) {
             throw new IOException("failed to open file");
